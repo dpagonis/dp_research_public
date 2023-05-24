a6df800749e249bf4567240df873135d592b5d64
HannahVerhaal <hjverhaal@gmail.com>
2023-05-24 13:46:44 -0600

    Fixed name of adjustguess

--- deconvolution/deconvolution.py
+++ deconvolution/deconvolution.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def DP_AdjGuess(wG, wE, NSmooth):
+def AdjGuess(wG, wE, NSmooth):
     if NSmooth == 0 or NSmooth == 1:
         wG = wG.astype(float)  # Convert wG to float data type
         wG -= wE
@@ -15,13 +15,4 @@ def DP_AdjGuess(wG, wE, NSmooth):
     wG = np.where(wG < 0, 0, wG)
     return wG
 
-# Sample inputs for testing
-wG = np.array([1, 2, 3, 4, 5])
-wE = np.array([0.5, 0.5, 0.1, 0.5, 0.5])
-NSmooth = 1
 
-# Call the function and store the modified wG
-modified_wG = DP_AdjGuess(wG, wE, NSmooth)
-
-# Print the modified wG array
-print(modified_wG)
